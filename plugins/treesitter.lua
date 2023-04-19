5dd09cb04096d39371abd439b6719cff7052b58e
Rodolfo Tolloi <rodolfo.tolloi@gmail.com>
2023-04-19 14:25:19 +0200

    some options for treesitter and mason

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "javascript", "typescript", "c", "lua",
+      "python", "vim", "vimdoc", "query" },
   },
 }
